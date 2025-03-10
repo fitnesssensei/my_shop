46bca014d9a51dce84448c99deaa79a2bf2bfba1
fitnesssensei <13fitnesssensei@gmail.com>
2025-03-10 10:09:19 +0300

    подготовка к деплою на рендер.ком

--- shop/settings.py
+++ shop/settings.py
@@ -165,12 +165,11 @@ MIDDLEWARE = [
     # ... остальные middleware ...
 ]
 
-# Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Static files (CSS, JavaScript, Images)
