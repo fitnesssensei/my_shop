791c87cd1a6e94fab033d1b5bc5f103f13b8272b
fitnesssensei <13fitnesssensei@gmail.com>
2025-03-13 11:56:27 +0300

    new commit

--- shop/urls.py
+++ shop/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-] + static(setting.MEDIA_URL, document_root=setting.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:  # Эта настройка работает только в режиме разработки (DEBUG = True)
